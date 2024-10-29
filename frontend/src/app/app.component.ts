a528a95a2a1ec91355376deaba36e1641d8e2cf9
Madu <kontakt.madu@gmail.com>
2024-10-29 11:51:53 +0100

    Changed to scss

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'maduhousehold';
