ea6fdc722ade352024fcf339f50a693705bfa502
Madu <kontakt.madu@gmail.com>
2024-10-30 18:17:39 +0100

    Language button and html lang attribute

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {DOCUMENT} from '@angular/common';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private htmlElement = inject(DOCUMENT).documentElement;
+  private translateSubscription: Subscription | undefined;
+
   constructor(private translate: TranslateService) { }
 
   ngOnInit() {
+    this.translateSubscription = this.translate.onDefaultLangChange.subscribe(langChangeEv => {
+      const lang = document.createAttribute('lang');
+      lang.value = langChangeEv.lang;
+      this.htmlElement.attributes.setNamedItem(lang);
+    });
     this.translate.setDefaultLang('en');
+  }
+
+  ngOnDestroy() {
+    this.translateSubscription?.unsubscribe();
   }
 }
--- frontend/src/app/layout/authentication-layout/authentication-layout.component.ts
+++ frontend/src/app/layout/authentication-layout/authentication-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-authentication-layout',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './authentication-layout.component.scss'
 })
 export class AuthenticationLayoutComponent {
+  languages: Map<string, string> = new Map<string, string>([
+    ['en', '🇺🇸'],
+    ['de', '🇩🇪'],
+    ['es', '🇪🇸'],
+    ['fr', '🇫🇷']
+  ]);
+  languagesKeys = Array.from(this.languages.keys());
+  currentLanguageIndex: number = 0;
+  flagSig = signal(this.languages.get(this.languagesKeys[this.currentLanguageIndex]));
 
+  constructor(public translateService: TranslateService) {}
+
+  switchLanguage() {
+    this.currentLanguageIndex++;
+    this.translateService.setDefaultLang(this.languagesKeys[this.currentLanguageIndex]);
+    this.flagSig.set(this.languages.get(this.languagesKeys[this.currentLanguageIndex]));
+    if (this.currentLanguageIndex + 1 === this.languagesKeys.length) {
+      this.currentLanguageIndex = -1;
+    }
+  }
 }
